1fc3bc840738f4e322f05a33b8d8f78b03776aba
ZenGx1n <1534408497@qq.com>
2021-01-03 11:01:59 +0800

    INIT

---
+++ src/student/score/manage/Score.java
@@ -0,0 +1,34 @@
+package student.score.manage;
+
+public class Score {
+    private String num;
+    private String name;
+    private double score;
+	
+    public Score(){
+		
+	}
+	public void setNum(String num) {
+		this.num = num;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setScore(double score) {
+		this.score = score;
+	}
+	
+	public String getNum() {
+		return num;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public double getScore() {
+		return score;
+	}
+}
